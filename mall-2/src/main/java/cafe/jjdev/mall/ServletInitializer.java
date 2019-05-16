e8a849e3cfec82dabbf827e1decefa2e789262b1
smart-202-10 <smart-202-10@smart-202-10-PC>
2019-05-16 09:16:29 +0900

    쇼핑몰 작업하는중 아직 미완성이지만 완성할수있다. 최선을 다해보자..

--- mall-2/src/main/java/cafe/jjdev/mall/ServletInitializer.java
+++ mall-2/src/main/java/cafe/jjdev/mall/ServletInitializer.java
@@ -7,8 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		
-		return application.sources(MallApplication.class);
+		return application.sources(Mall2Application.class);
 	}
 
 }
